9079bef92aa96407375a902e856a1154187df606
Nikolai Dale Milde <ndalemilde@outlook.com>
2020-02-11 20:46:58 +0100

    Created a simple reacted page

---
+++ precache-manifest.429ad69c66c860036606efbf00953819.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a80898779304e45c580cde58038d3ec0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6eb0030edcbbce00192f",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "f29a9d9d040369996972",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6eb0030edcbbce00192f",
+    "url": "/static/js/2.595d9d2a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.595d9d2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f29a9d9d040369996972",
+    "url": "/static/js/main.991bf906.chunk.js"
+  },
+  {
+    "revision": "c904a8467e0d4c0c8455",
+    "url": "/static/js/runtime-main.b535c537.js"
+  }
+]);
