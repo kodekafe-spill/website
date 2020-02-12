b21f9ef726b9b3f2cc00b5730d7f40c41862d2c5
Nikolai Dale Milde <ndalemilde@outlook.com>
2020-02-12 15:51:41 +0100

    Added a list

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,14 +2,20 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
   };
 
   render() {
     return (
-      <div className="App-header">
-        <span className={this.getBadgeClasses}>{this.formatCount()}</span>
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
   }
@@ -22,7 +28,7 @@ class Counter extends Component {
 
   formatCount() {
     const { count } = this.state;
-    return count === 0 ? "Zero" : this.state.count;
+    return count === 0 ? "Zero" : count;
   }
 }
 
